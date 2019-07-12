22fe59aa3c9cba86f53083aeb7f5c274a8114519
ivan13711 <ecolab07@gmail.com>
2019-07-12 23:46:58 +0300

    homeWork_snake_V_1

---
+++ 1v.js
@@ -0,0 +1,27 @@
+var picture[1]{
+  if (picture1.classList.contains("hidden")){
+     picture2 .classList.add("hidden");
+     picture3 .classList.add("hidden");
+   }
+   else {
+           picture2.classList.add("hidden");
+            picture3 .classList.add("hidden");
+     }
+     picture[2]{
+       if (picture2.classList.contains("hidden")){
+          picture1 .classList.add("hidden");
+          picture3 .classList.add("hidden");
+        }
+        else {
+                picture1.classList.add("hidden");
+                 picture3 .classList.add("hidden");
+          }
+          picture[3]{
+            if (picture3.classList.contains("hidden")){
+               picture1 .classList.add("hidden");
+               picture2 .classList.add("hidden");
+             }
+             else {
+                     picture1.classList.add("hidden");
+                      picture2 .classList.add("hidden");
+               }
